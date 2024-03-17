cacc56df0856cdf6d43975aa29e5f06126a64722
A_Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-03-17 11:05:03 +0100

    [NEEMQuery] renamed class instance.

--- test/test_neem_interface.py
+++ test/test_neem_interface.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from neem_query import neem_interface as nl
+from neem_query import neem_interface as ni
 from sqlalchemy import create_engine, Engine
 import pandas as pd
 
@@ -12,36 +12,36 @@ class TestNeemLoader(TestCase):
     def setUpClass(cls):
         # Connection to MariaDB NEEM database.
         cls.engine = create_engine('mysql+pymysql://newuser:password@localhost/test')
-        cls.all_neems_df = nl.get_dataframe_from_sql_query_file('../resources/get_pouring_neems.sql',
+        cls.all_neems_df = ni.get_dataframe_from_sql_query_file('../resources/get_pouring_neems.sql',
                                                                 cls.engine)
 
     def test_get_neem_ids(self):
-        neem_ids = nl.get_neem_ids(self.all_neems_df)
+        neem_ids = ni.get_neem_ids(self.all_neems_df)
         self.assertIsInstance(neem_ids, list)
         self.assertTrue(len(neem_ids) > 0)
 
     def test_filter_by_neem_id(self):
-        neem_ids = nl.get_neem_ids(self.all_neems_df)
-        neem_df = nl.filter_by_neem_id(self.all_neems_df, neem_ids[0])
+        neem_ids = ni.get_neem_ids(self.all_neems_df)
+        neem_df = ni.filter_by_neem_id(self.all_neems_df, neem_ids[0])
         self.assertIsInstance(neem_df, pd.DataFrame)
         self.assertTrue(len(neem_df) > 0)
 
     def test_filter_by_participant_type(self):
-        neem_df = nl.filter_by_participant_type(self.all_neems_df, 'soma:DesignedContainer')
+        neem_df = ni.filter_by_participant_type(self.all_neems_df, 'soma:DesignedContainer')
         self.assertIsInstance(neem_df, pd.DataFrame)
         self.assertTrue(len(neem_df) > 0)
 
     def test_get_participants(self):
-        participants = nl.get_participants(self.all_neems_df)
+        participants = ni.get_participants(self.all_neems_df)
         self.assertIsInstance(participants, list)
         self.assertTrue(len(participants) > 0)
 
     def test_get_task_sequence_of_neem(self):
-        df = nl.get_task_sequence_of_neem(2).get_result()
+        df = ni.get_task_sequence_of_neem(2).get_result()
         self.assertIsInstance(df, pd.DataFrame)
 
     def test_get_plan_of_neem(self):
-        df = nl.get_plan_of_neem(2).get_result()
+        df = ni.get_plan_of_neem(2).get_result()
         self.assertIsInstance(df, pd.DataFrame)
 
 
--- test/test_neem_query.py
+++ test/test_neem_query.py
@@ -7,52 +7,52 @@ from neem_query.neems_database import *
 
 
 class TestNeemSqlAlchemy(TestCase):
-    nl: NeemQuery
+    nq: NeemQuery
 
     def setUp(self):
-        self.nl = NeemQuery("mysql+pymysql://newuser:password@localhost/test")
+        self.nq = NeemQuery("mysql+pymysql://newuser:password@localhost/test")
 
     def tearDown(self):
-        self.nl.reset()
+        self.nq.reset()
 
     def test_sql_like(self):
-        tasks = (self.nl.session.query(DulExecutesTask).
+        tasks = (self.nq.session.query(DulExecutesTask).
                  filter(DulExecutesTask.dul_Task_o.like("%Pour%")).first())
         self.assertIsNotNone(tasks)
 
     def test_get_task_data(self):
-        task_data = self.nl.get_task_data("Pour", use_regex=True)
+        task_data = self.nq.get_task_data("Pour", use_regex=True)
         self.assertIsNotNone(task_data)
 
     def test_get_task_data_using_joins(self):
-        task_data = self.nl.get_task_data_using_joins("Pour", use_regexp=True)
+        task_data = self.nq.get_task_data_using_joins("Pour", use_regexp=True)
         self.assertIsNotNone(task_data)
 
     def test_join_task_participants(self):
-        df = (self.nl.select_from(DulExecutesTask).
+        df = (self.nq.select_from(DulExecutesTask).
               join_task_participants()).get_result()
         self.assertIsNotNone(df)
 
     def test_join_task_participant_types(self):
-        df = (self.nl.select_from(DulHasParticipant).
+        df = (self.nq.select_from(DulHasParticipant).
               join_participant_types()).get_result()
         self.assertIsNotNone(df)
 
     def test_join_task_types(self):
-        df = (self.nl.select(DulExecutesTask.dul_Task_o).
+        df = (self.nq.select(DulExecutesTask.dul_Task_o).
               join_task_types()).get_result()
         self.assertIsNotNone(df)
 
     def test_outer_join(self):
-        outer_df = (self.nl.select(DulExecutesTask.dul_Task_o).
+        outer_df = (self.nq.select(DulExecutesTask.dul_Task_o).
               join_task_participants(is_outer=True)).get_result()
-        self.nl.reset()
-        df = (self.nl.select(DulExecutesTask.dul_Task_o).
+        self.nq.reset()
+        df = (self.nq.select(DulExecutesTask.dul_Task_o).
               join_task_participants(is_outer=False)).get_result()
         self.assertIsNotNone(len(df) < len(outer_df))
 
     def test_multi_join(self):
-        df = (self.nl.select(TfHeader.stamp,
+        df = (self.nq.select(TfHeader.stamp,
                              ParticipantType.o.label("particpant")).
               select_tf_columns().
               select_tf_transform_columns().
@@ -71,9 +71,9 @@ class TestNeemSqlAlchemy(TestCase):
         self.assertIsNotNone(df)
 
     def test_get_neem(self):
-        neem = self.nl.session.query(Neem).first()
+        neem = self.nq.session.query(Neem).first()
         self.assertIsNotNone(neem)
 
     def test_get_neem_ids(self):
-        neem_ids = self.nl.session.query(Neem._id).all()
+        neem_ids = self.nq.session.query(Neem._id).all()
         self.assertIsNotNone(neem_ids)
