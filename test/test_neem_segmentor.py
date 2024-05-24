bad9109064b5f7d10aa5e068ae39cf4546f909a6
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-05-24 19:51:28 +0200

    [PyCRAMNEEMInterface] neem segmentor was created to segment actions. Contacts are detected successfult with the VR hand.

---
+++ test/test_neem_segmentor.py
@@ -0,0 +1,24 @@
+from neem_query.neem_pycram_interface import PyCRAMNEEMInterface
+from neem_query.neem_segmentor import NEEMSegmentor
+from unittest import TestCase
+
+from pycram.ros.viz_marker_publisher import VizMarkerPublisher
+
+
+class TestNEEMSegmentor(TestCase):
+    ns: NEEMSegmentor
+    viz_mark_publisher: VizMarkerPublisher
+
+    @classmethod
+    def setUpClass(cls):
+        pni = PyCRAMNEEMInterface('mysql+pymysql://newuser:password@localhost/test')
+        cls.ns = NEEMSegmentor(pni)
+        cls.viz_mark_publisher = VizMarkerPublisher()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.viz_mark_publisher._stop_publishing()
+        cls.ns.world.exit()
+
+    def test_detect_contacts_from_neem_motion_replay(self):
+        self.ns.detect_contacts_from_neem_motion_replay(14)
