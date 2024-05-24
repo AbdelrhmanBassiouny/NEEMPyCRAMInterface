d538f04c9b24646c94a63d59afd59b61dd2e87e9
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-05-24 16:47:36 +0200

    [PyCRAMNEEMInterface] replay motions test is working.
    # Conflicts:
    #	src/neem_query/neem_query.py

--- test/test_neem_pycram_interface.py
+++ test/test_neem_pycram_interface.py
@@ -191,3 +191,8 @@ class TestNeemPycramInterface(TestCase):
     @skip("Working but it is slow")
     def test_redo_grasping_action_for_neem(self):
         self.pni.redo_grasping_action(14)
+
+    @skip("Working but it is slow")
+    def test_motion_replay(self):
+        self.pni.query_neems_motion_replay_data().filter_by_task_types(['Pour'], regexp=True)
+        self.pni.replay_neem_motions()
